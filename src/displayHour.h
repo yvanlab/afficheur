ae7ee9a3e8c4ce30ab5a50d814d969e9281ad55e
LABRIT yvan <ylabrit@gmail.com>
2021-01-16 00:43:11 +0100

    mamange change discrepencies

--- src/displayHour.h
+++ src/displayHour.h
@@ -30,14 +30,6 @@
 
 */
 
-#define DATA_PIN 2
-//#define CLK_PIN   4
-#define LED_TYPE SK6812
-#define COLOR_ORDER RGB
-#define NUM_LEDS 86
-#define BRIGHTNESS 255
-
-
 class DisplayHour : public DisplayComponent
 {
 public:
@@ -46,7 +38,9 @@ public:
         HOUR = 0,
         POINT_HR = 1,
         MINUTE = 2,
-        /*POINT_MN=3, SECONDE=4,*/ LAST_HOUR_ELT = 3
+        POINT_MN=3, 
+        SECONDE=4,
+        LAST_HOUR_ELT = 3
     };
 
     DisplayHour(CRGB *firstPixel) : DisplayComponent(firstPixel)
@@ -73,12 +67,24 @@ public:
         CRGB *nextPixel = firstPixel;
         DEBUGLOG("DisplayDoubleDigit");
         m_listComponent.push_back(new DisplayDoubleDigit(nextPixel));
-        nextPixel = &nextPixel[DisplayDoubleDigit::getNbPixels()];
+        
         DEBUGLOG("DisplayPoints");
+        nextPixel = &nextPixel[DisplayDoubleDigit::getNbPixels()];
         m_listComponent.push_back(new DisplayPoints(nextPixel));
-        nextPixel = &nextPixel[DisplayPoints::getNbPixels()];
+        
         DEBUGLOG("DisplayDoubleDigit");
+        nextPixel = &nextPixel[DisplayPoints::getNbPixels()];
         m_listComponent.push_back(new DisplayDoubleDigit(nextPixel));
+        
+        DEBUGLOG("DisplayPoints");
+        nextPixel = &nextPixel[DisplayDoubleDigit::getNbPixels()];
+        m_listComponent.push_back(new DisplayPoints(nextPixel));
+
+        DEBUGLOG("DisplayDoubleDigit");
+        nextPixel = &nextPixel[DisplayPoints::getNbPixels()];
+        m_listComponent.push_back(new DisplayDoubleDigit(nextPixel));
+
+
     }
 
     void setValue(uint8_t iValue, HOUR_ELT elt)
@@ -86,8 +92,10 @@ public:
         m_listComponent[(uint8_t)elt]->setValue(iValue);
     };
 
-public:
-    CRGB m_leds[NUM_LEDS];
+    void setColorON(CRGB iValue, HOUR_ELT elt)
+    {
+        m_listComponent[(uint8_t)elt]->setColorON(iValue);
+    };
 
 };
 
